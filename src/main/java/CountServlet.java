f6bb6512aef89cb4bd78d4bc3287f179016e4836
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-04-20 14:57:28 -0500

    completed exercise

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -1,26 +1,29 @@
-import java.io.*;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+    private int counter = 0;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        String reset = request.getParameter("reset");
 
-        String name = request.getParameter("name");
+        counter++;
 
-        if (name == null) {
-            out.println("<h1>Hello, World!</h1>");
+        if (reset != null) {
+            counter = 0;
+            out.println("<h1>Counter reset. Total number of hits: " + counter);
         } else {
-            out.println("<h1>Hello, " + name + "!</h1>");
+            out.println("<h1>Total number of hits: " + counter);
         }
 
     }
