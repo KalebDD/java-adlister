3271fbcdf6371c24c88ea31ad8c6c6452ed42f19
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-05-01 12:11:04 -0500

    continuing exercise, usersdao seems complete. need to check user model

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -3,18 +3,17 @@ package com.codeup.adlister.dao;
 import com.codeup.adlister.models.User;
 import controllers.Config;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import java.sql.*;
 
 
 public class MySQLUsersDao implements Users {
 
+    private Connection connection = null;
+
     public MySQLUsersDao(Config config) {
         try {
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-            Connection connection = DriverManager.getConnection(
+            connection = DriverManager.getConnection(
                     config.getUrl(),
                     config.getUsername(),
                     config.getPassword()
@@ -26,11 +25,47 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public User findByUsername(String username) {
-        return null;
+        try {
+            String sql = "SELECT * FROM users WHERE username = ?";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, username);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                User result = new User(rs.getLong("id"), rs.getString("username"), rs.getString("email"), rs.getString("password"));
+                return result;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all users.", e);
+        }
     }
 
     @Override
     public Long insert(User user) {
-        return null;
+        try {
+            PreparedStatement stmt = connection.prepareStatement(createInsertQuery(user), Statement.RETURN_GENERATED_KEYS);
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new ad.", e);
+        }
     }
+
+    private String createInsertQuery(User user) {
+        return "INSERT INTO users(username, email, password) VALUES "
+                + "('" + user.getUsername() + "', "
+                + "'" + user.getEmail() +"', "
+                + "'" + user.getPassword() + "')";
+    }
+
+
+    public static void main(String[] args) {
+        DaoFactory.getUsersDao().findByUsername("testUser1");
+        User testUser2 = new User("testUser2", "testUser2@email.com", "testUser2Password");
+        DaoFactory.getUsersDao().insert(testUser2);
+    }
+
 }
